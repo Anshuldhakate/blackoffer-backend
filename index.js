8bbacb145a7597c82ea2e088b37ae90295509da6
unknown <anshuldhakate11@gmail.com>
2024-05-17 15:49:22 +0530

    done

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express= require("express")
 const connection= require("./data")
 const fs = require("fs");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app= express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("Welcome")
